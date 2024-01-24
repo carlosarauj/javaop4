6d3d2880e440b69452049636b456770c3d8e232c
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-01-23 22:40:45 -0300

    Add files via upload

---
+++ operadores03/src/operadores03/Principal.java
@@ -0,0 +1,33 @@
+package operadores03;
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		
+		num1 = 6;
+		num2 = 6;
+		
+		num1++;
+		++num2;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		num1++;
+		++num2;
+
+
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		int num3;
+		
+		num3 = 6;
+		num3 += 4;
+		
+		System.out.println(num3);
+	}
+
+}
